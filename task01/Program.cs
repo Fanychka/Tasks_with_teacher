a1a5955f8760d68a9d098cd9d08cd60a204af25b
PipBoy <pk7@ya.ru>
2022-09-08 05:41:13 +0700

    Task01_withTeacher_done

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,19 +1,9 @@
-﻿        Console.WriteLine("Введите первое число: ");
+﻿Console.WriteLine("Введите число: ");
 
-        string first_input = Console.ReadLine();
+string input = Console.ReadLine();
 
-        int first_number = int.Parse(first_input);
+int number = int.Parse(input);
 
-        Console.WriteLine("Введите второе число: ");
+int square = number * number;
 
-        string second_input = Console.ReadLine();
-        
-        int second_number = int.Parse(second_input);
-
-        int maxNumber = second_number;
-        int minNumber;
-        
-        if (second_number < first_number) maxNumber = first_number; minNumber = second_number;
-        if (second_number > first_number) minNumber = first_number;
-    
-        Console.WriteLine($"Большее число здесь {maxNumber}, а меньшее {minNumber}");
+Console.WriteLine($"Число {input} в квадрате равно {square}");
