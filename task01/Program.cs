87df9cd69b65f17b6333ddb0afd71adaadc5be52
PipBoy <pk7@ya.ru>
2022-09-08 04:10:47 +0700

    task01_with_teacher

---
+++ task01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+
+string input;
+
+input = Console.ReadLine();
+
+int number = int.Parse(input);
+
+int square = number * number;
+
+Console.WriteLine($"Число {input} в квадрате равно {square}");
